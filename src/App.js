09fdf3fbfd4b56e509458e800891e57915cfb512
Dwight Doane <dwightdoane@me.com>
2017-10-04 16:12:28 -0400

    commiting because it basically works, and i want to clean up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,120 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+
+import { p1mancala, p2mancala } from './reducers';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    const {
+      player1Row,
+      player2Row,
+      p1Score,
+      p2Score,
+      sew,
+      turn,
+      ended,
+      tallyScore,
+      winner,
+      reset,
+    } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1 className="current-player">current : {turn}</h1>
+        
+        <div className={`board row board--${ended ? 'ended' : ''}`}>
+          <div className={`player player--${turn !== 'player1' ? 'current' : ''}`}>
+            <p className="score">Player 2 score:&nbsp;{p2Score}</p>
+          </div>
+          <div className="rows">
+            <ul className="row row--player-2">
+              {player2Row.map(p => (
+                <li
+                  key={p.i}
+                  onClick={
+                    ended
+                      ? null
+                      : () =>
+                          sew({
+                            i: p.i - (turn === 'player2' ? 1 : 0),
+                            n: p.marbles,
+                          })
+                  }
+                >
+                  {p.marbles}
+                </li>
+              ))}
+            </ul>
+
+            <ul className="row">
+              {player1Row.map(p => (
+                <li
+                  key={p.i}
+                  onClick={ended ? null : () => sew({ i: p.i, n: p.marbles })}
+                >
+                  {p.marbles}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={`player player--${turn === 'player1' ? 'current' : ''}`}>
+            <p className="score">Player 1 score:&nbsp;{p1Score}</p>
+          </div>
+        </div>
+        {ended && winner.length ? (
+          <div className="game-over">
+            <h2>{winner}</h2><button onClick={reset}>Click to Reset</button>
+          </div>
+        ) : null}
+        {ended && !winner.length ? (
+          <div className="game-over" >Game over! <button onClick={tallyScore}>Click to tally score</button></div>
+        ) : null}
       </div>
     );
   }
 }
 
-export default App;
+// <pre>{JSON.stringify(this.props.board, null, 2)}</pre>
+const mapStateToProps = state => {
+  const board = Array.from(state.board);
+  return {
+    player1Row: board.slice(0, 6),
+    player2Row: board.slice(7, board.length - 1),
+    p1Score: board[p1mancala].marbles,
+    p2Score: board[p2mancala].marbles,
+    turn: state.turn,
+    board: state.board,
+    ended: state.ended,
+    winner: state.winner,
+  };
+};
+
+const sewPocket = ({ i, n }) => ({
+  type: 'SEW',
+  i,
+  n,
+});
+
+const tallyScore = () => ({
+  type: 'TALLY_SCORE',
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    sew(pocket) {
+      console.log('pocket', pocket);
+      dispatch(sewPocket(pocket));
+    },
+    tallyScore() {
+      dispatch(tallyScore());
+    },
+    reset() {
+      dispatch({
+        type: 'RESET',
+      });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
