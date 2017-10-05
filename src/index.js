0d309a68d50553f2d352251f9228d30332a2defe
Dwight Doane <dwightdoane@me.com>
2017-10-05 12:16:05 -0400

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import mancala from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(mancala);
+const store = createStore(
+  mancala,
+  compose(
+    typeof window === 'object' &&
+    typeof window.devToolsExtension !== 'undefined'
+      ? window.devToolsExtension()
+      : f => f
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
