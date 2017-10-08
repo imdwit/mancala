a38cb5e6d840c392057e92fb85caa16d533f0622
Dwight Doane <dwightdoane@me.com>
2017-10-07 20:05:45 -0400

    renamed it so it made sense

--- src/reducers/redo.spec.js
+++ src/reducers/redo.spec.js
@@ -3,44 +3,42 @@ import { board, increment, log, print, addMarbles, zeroAtIndex } from './redo';
 
 describe('Mancala', () => {
   describe('addMarbles', () => {
-
     test('increments from i to n', () => {
       const i = 0;
       const n = 4;
       const state = board.slice();
       const expected = [4, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0];
-      
+
       const actual = addMarbles(state, i, n);
-      
+
       expect(actual).toEqual(expected);
     });
-    
+
     test('if i + n is out of bounds, iterate from i to the end, then start at the begining', () => {
       const i = 10;
       const n = 8;
       const state = board.slice();
       //                                              i
       const expected = [5, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 5, 5, 1];
-      
+
       const actual = addMarbles(state, i, n);
-      
+
       expect(actual).toEqual(expected);
     });
-    
+
     test('do not increment the skipped index', () => {
       const i = 0;
       const n = 15;
       const state = board.slice();
       const skip = 6;
       const expected = [5, 6, 5, 5, 5, 5, 0, 5, 5, 5, 5, 5, 5, 1];
-      
+
       const actual = addMarbles(state, i, n, skip);
-      
+
       expect(actual).toEqual(expected);
     });
-    
-  })
-    describe('zerAtIndex', () => {
+  });
+  describe('zerAtIndex', () => {
     test('pocket at index 1 should be set to 0', () => {
       const i = 1;
       const state = board.slice(0, 6);
